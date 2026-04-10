85af0d6561931f071cda96d83bd482480e04a0d7
paul-artemyev <paul.artemyev@outlook.com>
2026-04-10 12:09:35 +0300

    echo2

--- 02-echo/main.c
+++ 02-echo/main.c
@@ -2,12 +2,20 @@
 #include<stdlib.h>
 #include<pico/stdlib.h>
 
+#define LED_PIN 25
+
 int main()
 {
     stdio_init_all();
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
     while (1)
     {
         char symbol = getchar();
         printf("received char: %c [ ASCII code: %d ]\n", symbol, symbol);
+        if(symbol == 'e') gpio_put(LED_PIN, 1);
+        if(symbol == 'v') gpio_put(LED_PIN, 0);
+        if(symbol == 'd') printf("led enable done\n");
     }
 }
